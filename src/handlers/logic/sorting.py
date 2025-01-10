53472726a88c5d4a7468374d7d77ea9886d30325
maniakalochka <prochorwolochoff@gmail.com>
2025-01-10 21:41:32 +0300

    Refactoring

---
+++ src/handlers/logic/sorting.py
@@ -0,0 +1,13 @@
+from dispatcher import dp
+from aiogram.types import Message
+from aiogram.filters import Command
+from extraction.reader import reader, filename
+from keyboards.reply_keyboards.learn_word_kb import LearnWordKeyboard
+
+
+@dp.message(Command("learn_words"))
+async def command_random_handler(message: Message) -> None:
+    random_word = await reader(filename)
+    keyboard = LearnWordKeyboard()
+    markup = keyboard.get_keyboard()
+    await message.answer(random_word, reply_markup=markup)
